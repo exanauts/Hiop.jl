9564d96975cde124691061285313401a739b85d4
Michel Schanen <mschanen@anl.gov>
2020-04-22 18:35:04 -0500

    fix

--- deps/test.c
+++ deps/test.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     cHiopProblem nlp;
-    hiop_createProblem(&nlp, 100);
+    hiop_createProblem(&nlp);
     hiop_solveProblem(&nlp);
     hiop_destroyProblem(&nlp);
     MPI_Finalize();
